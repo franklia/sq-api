d78b0ee5d21cd30826978d72c666d5fd94190029
Frank Liardet <frank.liardet@gmail.com>
2019-01-27 17:16:33 +1100

    Delete a question and update DOM

--- api/server.js
+++ api/server.js
@@ -2,18 +2,16 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import cors from 'cors';
 require('dotenv').config();
 import QuizQuestion from './models/quiz_questions';
 
 // create instances
 const app = express();
 const router = express.Router();
-// const corsOrigin = process.env.CORS_ORIGIN;
-
 
 // set our env variables
 const API_PORT = process.env.API_PORT || 3001;
-// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/learning_code';
 
 // connect to the database
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
@@ -28,11 +26,16 @@ mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+// Enable CORS
+app.use(cors({
+  origin: 'http://localhost:3000'
+}));
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN);
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 
 // Get all questions
 router.get('/questions/index', (req, res, next) => {
@@ -42,15 +45,14 @@ router.get('/questions/index', (req, res, next) => {
 });
 
 // Get one specific question
-router.get('/questions/:id', (req, res, next) => {
-  // res.send(req.params);
+router.get('/question/:id', (req, res, next) => {
   QuizQuestion.find({ _id: req.params.id })
     .then(data => res.json(data))
     .catch(next);
 });
 
 // Add a question
-router.post('/create', (req, res, next) => {
+router.post('/question/create', (req, res, next) => {
   if (req.body) {
     QuizQuestion.create(req.body)
       .then(data => res.json(data))
@@ -63,10 +65,27 @@ router.post('/create', (req, res, next) => {
 });
 
 // Update a question
-router.post('/question/:id', (req, res, next) => {
-  QuizQuestion.findByIdAndUpdate(req.params.id, req.body, (err, post) => {
-    if (err) return next(err);
-    res.json(post);
+router.post('/question/:id', (req, res) => {
+  QuizQuestion.findByIdAndUpdate(req.params.id, req.body, (err) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json('Successfully updated');
+    }
+  });
+});
+
+// Delete a question
+router.delete('/question/delete/:id', (req, res) => {
+  QuizQuestion.findByIdAndDelete({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.json({
+        reason: 'An error occurred, perhaps the id was not found?',
+        error: err
+      });
+    } else {
+      res.json('Successfully removed');
+    }
   });
 });
 
